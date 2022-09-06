6069dbeadfae751cc1eb418c0d20f96413dd5c1c
kimsj <sophia10047600@gmail.com>
2022-09-07 02:54:32 +0900

    12일차
    for, while 반복문 과제

---
+++ New.no.012/report2/index.js
@@ -0,0 +1,62 @@
+let playerSel = parseInt(prompt(`3.6.9 시~작!!`));
+let clapNum = parseInt(3, 6, 9);
+let maxNum = 0;
+// let tempNum = console.log("3", "6", "9");
+
+// for (let i = 1; i < playerSel; ) {
+//   const clapNum = ["3", "6", "9"];
+
+//   const maxNum = 1 + i;
+//   console.log(maxNum);
+//   if
+//   }
+// }
+
+for (let i = 1; i <= playerSel; ++i) {
+  const maxNum = i;
+  console.log(maxNum);
+  // const clapNum = parseInt(3, 6, 9);
+  if (i === 3) {
+    console.log("제발 짝!!!!!!");
+  } else if (i === 6) {
+    console.log("제발 짝!!!!!!");
+  } else if (i === 9) {
+    console.log("제발 짝!!!!!!");
+  }
+}
+
+// maxNum = [];
+
+// } else if (++i === clapNum) {
+//   console.log("6: 제발 짝!!!!!!");
+// }
+
+// 컴퓨터가 선택한 랜덤 수는 정수이기 때문에 (위의 코드 참고 / tempNum = parseInt(Math.random() * 10);)
+// 플레이어가 입력한 숫자 또한 정수로 정의한다.
+// 컴퓨터가 선택한 랜덤 수와 마찬가지로 중복되면 안되기 때문에 playerSel 배열에 숫자가 없는지 확인 후 추가한다.
+
+// const claparr = [3, 6, 9];
+// const clapNum = claparr.find((maxNum) => "짝");
+// console.log(clapNum);
+
+// tempReturn = arr.find(function (item) {
+//   console.log("item" + item);
+//   return item == 2;
+// });
+// if (maxNum.includes(clapNum)) {
+// console.log("제발 짝!!!!!!");
+// }
+// if (playerSel.includes(parseInt(clapNum))) {
+// const clapNum = parseInt[("3", "6", "9")];
+// console.log((clapNum = "짝"));
+// }
+
+// if (
+//   playerSel[0] == clapNum ||
+//   playerSel[1] == clapNum ||
+//   playerSel[2] == clapNum
+// )
+//   console.log("짝");
+
+// console.log("짝");
+// if (playerSel.includes(parseInt(clapNum))) playerSel.push(parseInt(clapNum));
