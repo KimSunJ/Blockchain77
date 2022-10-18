326e7b495e41902c6bef6a75deb38b0ef40ad63a
kimsj <sophia10047600@gmail.com>
2022-10-18 15:28:41 +0900

    1018_수업 ES6문법 server
    require

---
+++ New.No.030/console/test.js
@@ -0,0 +1,7 @@
+const consoleTest = require("./index");
+// .js는 생략 가능 js끼리만 인식하기 때문
+// require 노드, 라이브러리 모듈 이름을 가져온다.
+// 즉, 여기선 index.js의 global로 연결해준 값을 불러온다.
+console.log(a);
+
+// get, set을 가져오는 것은 closer
