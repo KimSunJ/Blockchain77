d6a5378fdc24fca9c527278f9e9c58ecc961b220
kimsj <sophia10047600@gmail.com>
2022-11-18 16:10:11 +0900

    1108 mysql

---
+++ New.No.038/models/table1.js
@@ -0,0 +1,61 @@
+const Sequelize = require("sequelize");
+
+module.exports = class Table1 extends Sequelize.Model {
+  // class *** : 클래스 선언
+  // extends : 상속, 오른쪽에 있는 걸 기본으로 왼쪽, 즉 지금 선언한 클래스를 생성한다.
+  // 오른쪽에 있는 내용은 전부 왼쪽에도 있다.
+  static init(sequelize) {
+    // 테이블 생성 / construct와 다름
+    return super.init(
+      {
+        // 첫번째 매개변수 / 컬럼들을 작성한다.
+        column1: {
+          type: Sequelize.STRING(10),
+          // type을 적을 때 왜 Sequelize에서 가져올까?
+          // number, string << 자바스크립트에 있는데 왜 가져올까?
+          // Javascript에 숫자가 들어왔을 때 정수인지 실수인지 알 수 있을까?
+          // << 없다, DB에서는 알아야한다.
+          // DB에서 사용하는 자료형에 맞게 Javascript의 자료형을 넣어주기 위해서 Sequelize 라이브러리에서 제공하는 자료형을 사용한다.
+        },
+        column2: {
+          // type: Sequelize.NUMBER,
+          // 얘는 index로 사용하지 못한다.//실수가 아니다.
+          // INT가 아니라 숫자다, FLOAT / DOUBLE 둘 중 하나다.
+          type: Sequelize.INTEGER.UNSIGNED,
+          // UNSIGNED는 무엇인가? 무조건 0보다 크다.
+          // 0 < number >> 음수가 없이 0과 양수로만 이루어져 있다.
+          // -20억 ~ 20억 / 0 ~ 40억 << 용량은 그대로이기 때문에 숫자를 원하는 만큼 더 사용할 수 있다. 어차피 인덱스는 1부터 사용할 것이기 때문에
+          primaryKey: true, // 테이블당 하나만 가능하다, 검색에 용이하다.
+          unique: true, // 데이터가 중복될 수 없다.
+          autoIncrement: true, //
+        },
+      },
+      {
+        //테이블에 대한 기본 설정
+        sequelize,
+        timestamps: true, // created_at, updated_at을 자동으로 추가한다.
+        underscored: true, // 카멜을 스네이크로 바꿀 것인가? 바꾼다(true) createdAt -> created_at
+        paranoid: false, // deleted_at을 추가한다. // delete 데이터를 삭제했을 때 DB에서 아예 없앨 것인지, 아니면 남길건지 결정해라 << true면 남긴다 -> deleted_at이 추가된다.
+        modelName: "Table1", // Javascript에서 사용하는 이름
+        tableName: "table1", // DB에 생성되는 테이블 이름
+        charset: "utf8mb4", // 언어 포멧
+        collate: "utf8mb4_general_ci", // 어떤 데이터 형식으로 저장할 것인가
+      }
+    );
+  }
+  static associate(db) {
+    // 관계를 위한 메서드
+    db.Table1.hasMany(db.Table2, {
+      // 많이 갖고있다. Table2를
+      foreignKey: "table1_column2",
+      // 연결하는 키
+      sourceKey: "column2",
+      // 상대가 저장할 키(컬럼)
+      as: "Table2s",
+      // 상대를 찾거나 추가하거나 등등에서 사용
+    });
+    db.Table1.belongsToMany(db.Table2, {
+      through: "link_tables",
+    });
+  }
+};
