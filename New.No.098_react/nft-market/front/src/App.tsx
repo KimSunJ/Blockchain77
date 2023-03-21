4d905c076acb2ff8428cdabfd5baa97a96b2732f
kimsj <sophia10047600@gmail.com>
2023-03-21 09:20:42 +0900

    nft_market 거래 민트

---
+++ New.No.098_react/nft-market/front/src/App.tsx
@@ -0,0 +1,33 @@
+import { List } from "./components/List";
+import { Mint } from "./components/Mint";
+import { useWeb3 } from "./modules/useWeb3";
+
+function App() {
+  const { chainId, account, web3, logIn } = useWeb3();
+  return (
+    <div>
+      <div>
+        {account && web3 ? (
+          <div>
+            <div>ChainId :{chainId} </div>
+            <div>Account: {account}</div>
+            <Mint account={account} web3={web3} />
+          </div>
+        ) : (
+          <div>
+            <button
+              onClick={() => {
+                logIn();
+              }}
+            >
+              MetaMask Log In
+            </button>
+          </div>
+        )}
+      </div>
+      <List account={account} />
+    </div>
+  );
+}
+
+export default App;
